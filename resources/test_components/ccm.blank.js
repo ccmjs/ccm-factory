a3d119cd22b9b80b3e10afc70382bf1ee4a9dbd6
LeonEck <eck.leon95@gmail.com>
2017-09-18 09:35:04 +0200

    - All resources are now local - Created test components

--- resources/test_components/ccm.blank.js
+++ resources/test_components/ccm.blank.js
@@ -1,52 +1,29 @@
+/**
+ * @overview blank template for a ccm component
+ * @author André Kless <andre.kless@web.de> 2017
+ * @license The MIT License (MIT)
+ */
+
 ( function () {
 
   var component = {
-    name: 'factory',
 
-    ccm: 'resources/ccm-10.0.0.min.js',
+    name: 'blank',
 
-    config: {
-      html: {
-        "main": {
-          "id": "main",
-          "inner": [
-            {
-              "inner": "URL der Komponente:"
-            },
-            {
-              "tag": "input",
-              "id": "componentURL"
-            },
-            {
-              "tag": "button",
-              "inner": "Start",
-              "onclick": "%%"
-            }
-          ]
-        }
-      }
-    },
+    ccm: 'https://akless.github.io/ccm/ccm.js',
 
     Instance: function () {
-      var self = this;
 
-      this.start = function () {
+      this.start = function ( callback ) {
 
-        var mainElement = this.ccm.helper.html(this.html.main, onClick);
+        this.element.innerHTML = 'Hello, World!';
 
-        this.element.appendChild(mainElement);
-
-        function onClick () {
-          var inputValue = mainElement.querySelector('#componentURL').value;
-
-          self.ccm.load(inputValue, function (returnData) {
-            console.log(returnData);
-          });
-        }
+        if ( callback ) callback();
       };
+
     }
+
   };
 
   function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
-
-}());
+}() );
