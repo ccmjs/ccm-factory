944077edd8167bb71468b7ee0288764b0a6c8022
LeonEck <eck.leon95@gmail.com>
2018-01-22 09:34:50 +0100

    New component can now be downloaded

--- ccm.factory.js
+++ ccm.factory.js
@@ -263,6 +263,17 @@
               "tag": "br"
             },
             {
+              "tag": "button",
+              "class": "btn btn-default",
+              "id": "downloadNewComponentButton",
+              "style": "display: none;",
+              "inner": "Download new component",
+              "onclick": "%downloadNewComponentClick%"
+            },
+            {
+              "tag": "br"
+            },
+            {
               "tag": "div",
               "id": "demoArea"
             }
@@ -372,7 +383,8 @@
           guidedEditingChosenClick: guidedEditingChosen,
           generateFromEditorClick: generateNewComponentFromEditor,
           showHelpForCCMURL: showHelpForCCMURL,
-          generateFromGuidedClick: generateNewComponentFromGuided
+          generateFromGuidedClick: generateNewComponentFromGuided,
+          downloadNewComponentClick: downloadNewComponent
         });
 
         this.element.appendChild(mainElement);
@@ -1412,6 +1424,7 @@
           mainElement.querySelector('#newComponentDisplay').style.display = 'block';
           mainElement.querySelector('#newComponentConfigDisplayLabel').style.display = 'block';
           mainElement.querySelector('#newComponentConfigDisplay').style.display = 'block';
+          mainElement.querySelector('#downloadNewComponentButton').style.display = 'block';
 
           mainElement.querySelector('#newComponentDisplay').value = generateNewComponentCode(newComponent);
 
@@ -1431,7 +1444,7 @@
             '  var component = ';
           const componentEnding = ';\n' +
             '\n' +
-            '  function p(){window.ccm[v].component(component)}const f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{const n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"===typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{const e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}\n' +
+            '  function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}\n' +
             '}';
 
           return componentBeginning + innerPartOfCompoment + componentEnding;
@@ -1476,6 +1489,23 @@
           document.body.appendChild(demoHtmlTag);
 
           eval(generateNewComponentCode(demoNewComponent));
+        }
+
+        /**
+         * Downloads the newly generated component
+         */
+        function downloadNewComponent() {
+          const blob = new Blob( [ generateNewComponentCode(newComponent) ], { type: 'text/plain' } );
+          const ankerElement = document.createElement('a');
+          ankerElement.href = URL.createObjectURL(blob);
+          ankerElement.download ='ccm.' + newComponent.name + '.js';
+
+          ankerElement.style.display = 'none';
+          document.body.appendChild(ankerElement);
+
+          ankerElement.click();
+
+          document.body.removeChild(ankerElement);
         }
 
         /**
@@ -1572,6 +1602,6 @@
     }
   };
 
-  function p(){window.ccm[v].component(component)}const f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{const n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"===typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{const e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
+  function p(){window.ccm[v].component(component)}var f="ccm."+component.name+(component.version?"-"+component.version.join("."):"")+".js";if(window.ccm&&null===window.ccm.files[f])window.ccm.files[f]=component;else{var n=window.ccm&&window.ccm.components[component.name];n&&n.ccm&&(component.ccm=n.ccm),"string"==typeof component.ccm&&(component.ccm={url:component.ccm});var v=component.ccm.url.split("/").pop().split("-");if(v.length>1?(v=v[1].split("."),v.pop(),"min"===v[v.length-1]&&v.pop(),v=v.join(".")):v="latest",window.ccm&&window.ccm[v])p();else{var e=document.createElement("script");document.head.appendChild(e),component.ccm.integrity&&e.setAttribute("integrity",component.ccm.integrity),component.ccm.crossorigin&&e.setAttribute("crossorigin",component.ccm.crossorigin),e.onload=function(){p(),document.head.removeChild(e)},e.src=component.ccm.url}}
 
 }
