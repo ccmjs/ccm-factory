844530395291603012d6100fe11f8be35aed99c7
LeonEck <eck.leon95@gmail.com>
2018-04-25 07:57:34 +0200

    Changed build to point to new repository

--- build.js
+++ build.js
@@ -10,10 +10,10 @@ fs.readFile('ccm.factory.js', 'utf8', function (err, data) {
   }
 
   const result = data
-    .replace(/js\/jsonfn.js/g, 'https://leoneck.github.io/ccm-factory/js/jsonfn.js')
-    .replace(/\/js\/webcomponents-lite.js/g, 'https://leoneck.github.io/ccm-factory/js/webcomponents-lite.js')
-    .replace(/js\/juicy-ace-editor.html/g, 'https://leoneck.github.io/ccm-factory/js/juicy-ace-editor.html')
-    .replace(/css\/default.css/g, 'https://leoneck.github.io/ccm-factory/css/default.css')
+    .replace(/js\/jsonfn.js/g, 'https://ccmjs.github.io/ccm-factory/js/jsonfn.js')
+    .replace(/\/js\/webcomponents-lite.js/g, 'https://ccmjs.github.io/ccm-factory/js/webcomponents-lite.js')
+    .replace(/js\/juicy-ace-editor.html/g, 'https://ccmjs.github.io/ccm-factory/js/juicy-ace-editor.html')
+    .replace(/css\/default.css/g, 'https://ccmjs.github.io/ccm-factory/css/default.css')
     .replace(/css\/bootstrap.min.css/g, 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css')
     .replace(/js\/ccm-14.3.0.js/g, 'https://akless.github.io/ccm/version/ccm-14.3.0.min.js')
     .replace(/name: 'factory',/g, 'name: \'factory\',version: [' + versionArray[0] + ',' + versionArray[1] + ',' + versionArray[2] + '],');
